e69ccd837e9278372866762543fd10d6e3ff45b8
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-09-09 18:15:22 +0100

    Fixing Overflow

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,17 +5,16 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/index.jsx';
 const Layout = () => (
 
-    <div className='App flex flex-col justify-between h-screen'>
-        <div>
+    <div className='App flex flex-col justify-between w-full h-screen overflow-x-hidden'>
+        <div className=''>
         <h5 className='tags top-tags py-2 pt-4' >&lt;html&gt; </h5>
            <h5 className='tags top-tags py-2 ' >&nbsp;&nbsp;&nbsp;&nbsp;&lt;body&gt; </h5>
         </div>
-     
-        <Sidebar />
+             <Sidebar />
         <div className='page'>
             <Outlet />       
         </div>
-        <div>
+        <div className='mb-16'>
         <h5 className='tags top-tags py-2 '>&nbsp;&nbsp;&nbsp;&nbsp; &lt;/body&gt; </h5>
         <h5 className='tags top-tags py-2 pt-4'>&lt;/html&gt; </h5>
         </div>
